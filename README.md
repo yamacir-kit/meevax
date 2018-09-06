2c6cb2220bf59fc1dd7efb29d9ddaca474913429
yamacir-kit <httperror@404-notfound.jp>
2018-09-06 12:49:29 +0900

    Add README.md.cmake

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # meevax
 
+Version 0.0.3 in development.
+
+## Dependency
+
+- C++17
+- Boost C++ Libraries 1.58.0
+
---
+++ configure/README.md.cmake
@@ -0,0 +1,9 @@
+# meevax
+
+Version ${PROJECT_VERSION} in development.
+
+## Dependency
+
+- C++17
+- Boost C++ Libraries ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}
+
