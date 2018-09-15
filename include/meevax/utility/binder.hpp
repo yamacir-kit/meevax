da7097b1ef8e6c3bdd52e11e5ba1e9aecc10b120
yamacir-kit <httperror@404-notfound.jp>
2018-09-16 00:54:46 +0900

    Add header directory "facade"

--- include/meevax/utility/binder.hpp
+++ include/meevax/utility/binder.hpp
@@ -5,22 +5,22 @@
 #include <typeinfo>
 #include <utility>
 
-#include <meevax/utility/conditionally_trivial_destructible.hpp>
+#include <meevax/facade/conditionally_trivial_destructible.hpp>
 
 namespace meevax::utility
 {
   template <typename T, typename U>
   struct binder
-    : public conditionally_trivial_destructible<T>,
-      public conditionally_trivial_destructible<U>
+    : public facade::conditionally_trivial_destructible<T>,
+      public facade::conditionally_trivial_destructible<U>
   {
     explicit constexpr binder(const T& value)
-      : conditionally_trivial_destructible<T> {value}
+      : facade::conditionally_trivial_destructible<T> {value}
     {}
 
     template <typename... Ts>
     explicit constexpr binder(Ts&&... xs)
-      : conditionally_trivial_destructible<T> {std::forward<Ts>(xs)...}
+      : facade::conditionally_trivial_destructible<T> {std::forward<Ts>(xs)...}
     {}
 
     auto type() const noexcept
