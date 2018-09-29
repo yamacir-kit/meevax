64d966b003136b4dee999c7d018583c8c9e238aa
yamacir-kit <httperror@404-notfound.jp>
2018-09-29 22:36:19 +0900

    Update class cell to inherit std::pair
    これで名実共にペアを表現した型になった
    ゆくゆくは連想型としてのセルを型としてサポートしたい

--- include/meevax/lisp/cell.hpp
+++ include/meevax/lisp/cell.hpp
@@ -14,6 +14,7 @@
 #include <meevax/lisp/alias.hpp>
 #include <meevax/lisp/error.hpp>
 #include <meevax/utility/binder.hpp>
+// #include <meevax/utility/recursive_iterator.hpp>
 
 namespace meevax::lisp
 {
@@ -28,7 +29,7 @@ namespace meevax::lisp
     using iterator_category = std::forward_iterator_tag;
 
     template <typename... Ts>
-    constexpr cursor(Ts&&... args) noexcept
+    constexpr cursor(Ts&&... args)
       : std::shared_ptr<cell> {std::forward<Ts>(args)...}
     {}
 
@@ -36,27 +37,35 @@ namespace meevax::lisp
     cursor operator++() noexcept;
   };
 
+  // auto car = [](auto&& e) { return std::get<0>(*e); };
+  // auto cdr = [](auto&& e) { return std::get<1>(*e); };
+  //
+  // using cursor = meevax::utility::recursive_iterator<cell, car, cdr>;
+
   cursor nil {nullptr};
 
   class cell
+    : public std::pair<cursor, cursor>
   {
     // Operation is possible only via class cursor.
     friend class cursor;
 
-    cursor car_, cdr_;
+    // cursor car_, cdr_;
 
   public:
-    constexpr cell() noexcept = default;
+    constexpr cell() = default;
 
     template <typename T>
-    cell(T&& car)
-      : car_ {std::forward<T>(car)}
+    constexpr cell(T&& car)
+      // : car_ {std::forward<T>(car)}
+      : std::pair<cursor, cursor> {std::forward<T>(car), nil}
     {}
 
     template <typename T, typename U>
     cell(T&& car, U&& cdr)
-      : car_ {std::forward<T>(car)},
-        cdr_ {std::forward<U>(cdr)}
+      // : car_ {std::forward<T>(car)},
+      //   cdr_ {std::forward<U>(cdr)}
+      : std::pair<cursor, cursor> {std::forward<T>(car), std::forward<U>(cdr)}
     {}
 
   public:
@@ -67,7 +76,7 @@ namespace meevax::lisp
     }
     catch (const std::bad_cast& error)
     {
-      std::cerr << error("arbitrary dispatch failed for (" << car_ << " . " << cdr_ << ")") << std::endl;
+      std::cerr << error("arbitrary dispatch failed for (" << first << " . " << second << ")") << std::endl;
       std::exit(boost::exit_exception_failure);
     }
 
@@ -112,26 +121,27 @@ namespace meevax::lisp
   }
   catch (const std::out_of_range& error)
   {
-    std::cerr << error("boolean dispatch failed for " << e) << std::endl;
+    std::cerr << error("atom dispatch failed for " << e) << std::endl;
     std::exit(boost::exit_exception_failure);
   }
 
   inline cursor cursor::operator*() const noexcept
   {
-    return get()->car_;
+    // return get()->car_;
+    return get()->first;
   }
 
   inline cursor cursor::operator++() noexcept
   {
-    return *this = get()->cdr_;
+    // return *this = get()->cdr_;
+    return *this = get()->second;
   }
 
   template <typename T, typename... Ts>
   cursor make_as(Ts&&... args)
   {
-    return std::make_shared<
-             meevax::utility::binder<T, cell>
-           >(std::forward<Ts>(args)...);
+    using binder = meevax::utility::binder<T, cell>;
+    return std::make_shared<binder>(std::forward<Ts>(args)...);
   }
 
   auto cons = [](auto&&... args) -> cursor
