047b0371f7060f25fe3a0163c8014c7ce941ec15
yamacir-kit <httperror@404-notfound.jp>
2018-11-01 04:44:53 +0900

    Remove dependency to "conditionally_trivial_destructible"
    is_trivial_destructibleの意味を完全に間違えていた。
    恥ずかしい。

--- include/meevax/utility/binder.hpp
+++ include/meevax/utility/binder.hpp
@@ -4,8 +4,6 @@
 #include <typeinfo>
 #include <utility>
 
-#include <meevax/facade/conditionally_trivial_destructible.hpp>
-
 // TODO
 // デフォルト引数用プレースホルダクラスと基底クラスの追加
 // 基底クラス作成の簡易化のためのファサードクラスの追加
@@ -14,20 +12,12 @@ namespace meevax::utility
 {
   template <typename T, typename Base>
   struct binder
-    // : public facade::conditionally_trivial_destructible<T>,
     : public T,
       public Base
   {
-    // using bound = facade::conditionally_trivial_destructible<T>;
-    using bound = T;
-
-    explicit constexpr binder(const T& value)
-      : bound {value}
-    {}
-
     template <typename... Ts>
     explicit constexpr binder(Ts&&... xs)
-      : bound {std::forward<Ts>(xs)...}
+      : T {std::forward<Ts>(xs)...}
     {}
 
     auto type() const noexcept
