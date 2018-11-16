f2088e7e03aeb40d225ec291f3b04ecda6a6797e
yamacir-kit <httperror@404-notfound.jp>
2018-11-16 22:29:57 +0900

    Finei-tune internal misc functions

--- include/meevax/lisp/list.hpp
+++ include/meevax/lisp/list.hpp
@@ -20,6 +20,27 @@ namespace meevax::lisp
   tuple::accessor<0> car {};
   tuple::accessor<1> cdr {};
 
+  // TODO move to symbol table utility header
+  decltype(auto) intern(const std::string& s, std::unordered_map<std::string, cursor>& table)
+  {
+    if (const auto iter {table.find(s)}; iter != std::end(table))
+    {
+      return iter->second;
+    }
+    else return table.emplace(
+      s, std::make_shared<utility::binder<std::string, cell>>(s)
+    ).first->second;
+  }
+
+  // TODO move to symbol table utility header
+  // returns unchecked reference
+  decltype(auto) lookup(const std::string& s, const std::unordered_map<std::string, cursor>& table)
+  {
+    const auto iter {table.find(s)};
+    assert(iter != std::end(table));
+    return iter->second;
+  }
+
   auto cons = [](auto&& head, auto&& tail)
     -> cursor
   {
@@ -33,7 +54,7 @@ namespace meevax::lisp
   }
 
   template <typename T>
-  bool atom(T&& e)
+  decltype(auto) atom(T&& e)
   {
     static const std::unordered_map<std::type_index, bool> dispatch
     {
@@ -46,12 +67,12 @@ namespace meevax::lisp
 
   auto list = [](auto&&... args) constexpr
   {
-    return (args | ... | symbols["nil"]);
+    return (args | ... | lookup("nil", symbols));
   };
 
   decltype(auto) length(const cursor& exp)
   {
-    return std::distance(exp, symbols["nil"]);
+    return std::distance(exp, lookup("nil", symbols));
   }
 
   cursor append(const cursor& x, const cursor& y)
@@ -59,11 +80,11 @@ namespace meevax::lisp
     return !x ? y : car(x) | append(cdr(x), y);
   }
 
-  cursor zip(cursor x, cursor y)
+  cursor zip(const cursor& x, const cursor& y)
   {
     if (!x && !y)
     {
-      return symbols["nil"];
+      return lookup("nil", symbols);
     }
     else if (!atom(x) && !atom(y))
     {
@@ -71,35 +92,25 @@ namespace meevax::lisp
     }
     else
     {
-      return symbols["nil"];
+      return lookup("nil", symbols);
     }
   }
 
-  cursor lookup(cursor var, cursor env)
+  cursor lookup(const cursor& var, const cursor& env)
   {
-    return !var || !env ? symbols["nil"]
-                        : var == caar(env) ? cadar(env)
-                                           : lookup(var, cdr(env));
-  };
-
-  decltype(auto) intern(const std::string& s, std::unordered_map<std::string, cursor>& table)
-  {
-    if (const auto iter {table.find(s)}; iter != std::end(table))
+    if (!var || !env)
     {
-      return iter->second;
+      return lookup("nil", symbols);
     }
-    else return table.emplace(
-      s, std::make_shared<utility::binder<std::string, cell>>(s)
-    ).first->second;
-  }
-
-  // returns unchecked reference
-  decltype(auto) lookup(const std::string& s, std::unordered_map<std::string, cursor>& table)
-  {
-    const auto iter {table.find(s)};
-    assert(iter != std::end(table));
-    return iter->second;
-  }
+    else if (caar(env) == var)
+    {
+      return cadar(env);
+    }
+    else
+    {
+      return lookup(var, cdr(env));
+    }
+  };
 } // namespace meevax::lisp
 
 #endif // INCLUDED_MEEVAX_LISP_LIST_HPP
