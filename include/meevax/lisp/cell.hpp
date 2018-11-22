3dc5023b26c9bbec143597b77c3d9d49d18ed184
yamacir-kit <httperror@404-notfound.jp>
2018-11-23 02:45:38 +0900

    Update tuple iterator to independent from std::shared_ptr

--- include/meevax/lisp/cell.hpp
+++ include/meevax/lisp/cell.hpp
@@ -1,6 +1,7 @@
 #ifndef INCLUDED_MEEVAX_LISP_CELL_HPP
 #define INCLUDED_MEEVAX_LISP_CELL_HPP
 
+#include <memory>
 #include <string>
 #include <tuple>
 #include <unordered_map>
@@ -11,9 +12,8 @@
 
 namespace meevax::lisp
 {
-  struct cell;
-
-  using cursor = tuple::iterator<cell>;
+  struct cell; // forward decreation for type `cusror`
+  using cursor = tuple::iterator<std::shared_ptr<cell>>;
 
   struct cell
     : public std::tuple<cursor, cursor>,
