4e74a8e1990eee507439c892510eaf7bb78fa5f2
yamacir-kit <httperror@404-notfound.jp>
2018-11-24 21:10:41 +0900

    Move contents of header "procedure.hpp" to "evaluator.hpp"
    よくよく考えればクラス内部に今後も増えると思われる定義がつらつら
    書いてあるのが邪魔だっただけだから、
    下の方に逃がしてやれば十分だと気づいた。

--- include/meevax/lisp/evaluator.hpp
+++ include/meevax/lisp/evaluator.hpp
@@ -37,11 +37,7 @@ namespace meevax::lisp
     cursor env_;
 
   public:
-    evaluator()
-      : env_ {nil}
-    {
-      #include <meevax/lisp/procedure.hpp>
-    }
+    evaluator(); // The definition is at the end of this file.
 
     template <typename Expression>
     constexpr decltype(auto) operator()(Expression&& exp)
@@ -95,6 +91,84 @@ namespace meevax::lisp
       return !exp ? nil : evaluate(car(exp), env) | evlis(cdr(exp), env);
     }
   };
+
+  evaluator::evaluator()
+    : env_ {nil}
+  {
+    define("quote", [](auto&& exp, auto)
+    {
+      return cadr(exp);
+    });
+
+    define("atom", [&](auto&& exp, auto&& env)
+    {
+      return atom(evaluate(cadr(exp), env)) ? t : nil;
+    });
+
+    define("eq", [&](auto&& exp, auto&& env)
+    {
+      return evaluate(cadr(exp), env) == evaluate(caddr(exp), env) ? t : nil;
+    });
+
+    define("if", [&](auto&& exp, auto&& env)
+    {
+      return evaluate(
+        evaluate(cadr(exp), env) ? caddr(exp) : cadddr(exp),
+        env
+      );
+    });
+
+    define("cond", [&](auto&& exp, auto&& env)
+    {
+      const auto buffer {
+        std::find_if(cdr(exp), nil, [&](auto iter)
+        {
+          return evaluate(car(iter), env);
+        })
+      };
+      return evaluate(cadar(buffer), env);
+    });
+
+    define("car", [&](auto&& exp, auto&& env)
+    {
+      return car(evaluate(cadr(exp), env));
+    });
+
+    define("cdr", [&](auto&& exp, auto&& env)
+    {
+      return cdr(evaluate(cadr(exp), env));
+    });
+
+    define("cons", [&](auto&& exp, auto&& env)
+    {
+      return evaluate(cadr(exp), env) | evaluate(caddr(exp), env);
+    });
+
+    define("lambda", [&](auto&& exp, auto&& env)
+    {
+      using binder = utility::binder<closure, cell>;
+      return std::make_shared<binder>(exp, env);
+    });
+
+    define("define", [&](auto&& var, auto)
+    {
+      return assoc(cadr(var), env_ = list(cadr(var), caddr(var)) | env_);
+    });
+
+    define("list", [&](auto&& exp, auto&& env)
+    {
+      return lambda::y([&](auto&& proc, auto&& exp, auto&& env) -> cursor
+      {
+        return evaluate(car(exp), env) | (cdr(exp) ? proc(proc, cdr(exp), env) : nil);
+      })(cdr(exp), env);
+    });
+
+    define("exit", [](auto, auto)
+      -> cursor
+    {
+      std::exit(boost::exit_success);
+    });
+  }
 } // namespace meevax::lisp
 
 #endif // INCLUDED_MEEVAX_LISP_EVALUATOR_HPP
