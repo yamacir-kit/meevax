09ea7a09a02e8a65062631a2c0255d113c8bf75d
yamacir-kit <httperror@404-notfound.jp>
2019-01-31 02:36:46 +0900

    Disable accessor stack supports

--- include/meevax/core/accessor.hpp
+++ include/meevax/core/accessor.hpp
@@ -63,31 +63,31 @@ namespace meevax::core
     }
 
   public: // stack supports
-    using size_type = std::size_t;
-
-    using reference = accessor<T>&;
-    using const_reference = const reference;
-
-    decltype(auto) back() noexcept
-    {
-      return *this ? operator*() : *this;
-    }
-
-    decltype(auto) push_back(const accessor<T>& access)
-    {
-      return *this = std::make_shared<T>(access, *this);
-    }
-
-    decltype(auto) pop_back()
-    {
-      return operator++();
-    }
-
-    template <typename... Ts>
-    decltype(auto) emplace_back(Ts&&... args)
-    {
-      return *this = std::make_shared<T>(std::forward<Ts>(args)..., *this);
-    }
+    // using size_type = std::size_t;
+    //
+    // using reference = accessor<T>&;
+    // using const_reference = const reference;
+    //
+    // decltype(auto) back() noexcept
+    // {
+    //   return *this ? operator*() : *this;
+    // }
+    //
+    // decltype(auto) push_back(const accessor<T>& access)
+    // {
+    //   return *this = std::make_shared<T>(access, *this);
+    // }
+    //
+    // decltype(auto) pop_back()
+    // {
+    //   return operator++();
+    // }
+    //
+    // template <typename... Ts>
+    // decltype(auto) emplace_back(Ts&&... args)
+    // {
+    //   return *this = std::make_shared<T>(std::forward<Ts>(args)..., *this);
+    // }
   };
 } // namespace meevax::core
 
