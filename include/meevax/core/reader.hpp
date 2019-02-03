2fa15a49f32ad0712b4a88f5b418d51037da24e2
yamacir-kit <httperror@404-notfound.jp>
2019-02-04 01:54:23 +0900

    Cleanup reader

--- include/meevax/core/reader.hpp
+++ include/meevax/core/reader.hpp
@@ -70,39 +70,13 @@ namespace meevax::core
     template <template <typename...> typename SequenceContainer>
     decltype(auto) operator()(const SequenceContainer<std::string>& tokens)
     {
-      // return parse(std::cbegin(tokens), std::cend(tokens)).generate();
       return operator()(std::cbegin(tokens), std::cend(tokens));
     }
 
   protected:
-    // TODO Rename constractor?
-    struct [[deprecated]] abstract_syntax_tree
-      : public std::list<abstract_syntax_tree>
-    {
-      cursor value;
-
-      abstract_syntax_tree(const cursor& value = nil)
-        : value {value}
-      {}
-
-      cursor generate() const
-      {
-        if (std::empty(*this)) // is atomic value
-        {
-          return value;
-        }
-        // Following weird code performs "fold-right" algorithm.
-        else return std::accumulate(std::rbegin(*this), std::rend(*this), nil, [&](auto&& rhs, auto&& lhs)
-        {
-          return cons(lhs.generate(), rhs);
-        });
-      }
-    };
-
     template <typename InputIterator>
     cursor list_(InputIterator&& iter, InputIterator&& end)
     {
-      // std::cerr << "        sequence" << std::endl;
       if (*iter == "(")
       {
         auto buffer {operator()(iter, end)};
@@ -118,48 +92,26 @@ namespace meevax::core
     template <typename InputIterator>
     cursor operator()(InputIterator&& iter, InputIterator&& end)
     {
-      // std::cerr << "[debug] parsing {";
-      // for (auto hoge {iter}; hoge != end; ++hoge)
-      // {
-      //   std::cerr << "\x1B[33m" << *hoge << (std::next(hoge) != end ? "\x1B[0m, " : "\x1B[0m}\n");
-      // }
-      //
-      // if (std::distance(iter, end) == 0)
-      // {
-      //   std::cerr << "!!!" << std::endl;
-      //   return nil;
-      // }
-      // else
       switch (auto token {*iter}; token[0])
       {
       case '(':
-        // std::cerr << "        open" << std::endl;
         if (auto&& car {operator()(++iter, end)}; !car)
         {
-          // std::cerr << "        car is nil" << std::endl;
           return nil;
         }
         else if (*++iter != ".")
-        // if (auto&& car {operator()(++iter, end)}; *++iter != ".")
         {
-          // std::cerr << "        list" << std::endl;
-          return cons(
-            std::forward<decltype(car)>(car),
-            list_(std::forward<InputIterator>(iter), std::forward<InputIterator>(end))
-          );
+          return cons(car, list_(iter, end));
         }
         else
         {
-          // std::cerr << "        pair" << std::endl;
-          return cons(std::forward<decltype(car)>(car), operator()(++iter, end));
+          return cons(car, operator()(++iter, end));
         }
 
       case ')':
-        // std::cerr << "        close" << std::endl;
         return nil;
 
       case '\'':
-        // std::cerr << "        quote" << std::endl;
         return list(package->intern("quote"), operator()(++iter, end));
 
       case '`':
@@ -191,99 +143,10 @@ namespace meevax::core
         }
         catch (const std::runtime_error&) // is not number
         {
-          // std::cerr << "        symbol" << std::endl;
           return package->intern(*iter);
         }
       }
     }
-
-    // Recursive Descent Parser
-    template <typename InputIterator>
-    [[deprecated]] auto parse(InputIterator&& iter, InputIterator&& end)
-      -> abstract_syntax_tree
-    {
-      abstract_syntax_tree buffer {};
-
-      // if (std::distance(iter, end) != 0) // XXX Removable?
-      // {
-        if (*iter == "(") while (++iter != end && *iter != ")")
-        {
-          buffer.emplace_back(parse(iter, end)); // TODO Able to convert constructor?
-        }
-        else
-        {
-          switch ((*iter)[0])
-          {
-          case '\'':
-            buffer.emplace_back(package->intern("quote"));
-            buffer.emplace_back(parse(++iter, end));
-            break;
-
-          case '`':
-            buffer.emplace_back(package->intern("quasiquote"));
-            buffer.emplace_back(parse(++iter, end));
-            break;
-
-          case '#':
-            return expand_macro(++iter, end);
-
-          default:
-            try // XXX Dirty hack!!!
-            {
-              return {cursor::bind<number>(*iter)};
-            }
-            catch (const std::runtime_error&) // is not number
-            {
-              return package->intern(*iter);
-            }
-          }
-        }
-      // }
-
-      return buffer;
-    }
-
-    template <typename InputIterator>
-    [[deprecated]] auto expand_macro(InputIterator&& iter, InputIterator&& end)
-      -> abstract_syntax_tree
-    {
-      if (std::distance(iter, end) != 0)
-      {
-        if (*iter == "(")
-        {
-          auto buffer {parse(iter, end)};
-          buffer.emplace_front(package->intern("vector"));
-          return buffer;
-        }
-        else switch ((*iter)[0])
-        {
-        case 't':
-          return true_v;
-
-        case 'f':
-          return false_v;
-
-        // case 'x':
-        //   return {cursor::bind<number>("0x" + std::string {std::begin(*iter) + 1, std::end(*iter)})};
-
-        default:
-          throw std::runtime_error {"unknown reader macro #" + *iter};
-        }
-      }
-      else
-      {
-        throw std::runtime_error {"reader dispatch macro requires at least one character as argument"};
-      }
-    }
-  };
-
-  [[deprecated]] auto read = [](auto&& context, auto&& tokens)
-  {
-    // if (const auto tokens {tokenize(s)}; balance(tokens) <= 0)
-    // {
-      return syntax_tree {tokens}.compile(std::forward<decltype(context)>(context));
-    // }
-    // else throw s;
   };
 } // namespace meevax::core
 
