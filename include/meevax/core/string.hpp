4226479cb668b7e080b8c4d6396973f1a4420794
yamacir-kit <httperror@404-notfound.jp>
2019-02-20 04:42:01 +0900

    Add character and string type (unused)
    ヘッダを追加しただけ

---
+++ include/meevax/core/string.hpp
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_MEEVAX_CORE_STRING_HPP
+#define INCLUDED_MEEVAX_CORE_STRING_HPP
+
+#include <meevax/character/unicode.hpp>
+#include <meevax/core/pair.hpp>
+
+namespace meevax::core
+{
+  struct string
+    : public virtual pair
+  {
+  };
+} // namespace meevax::core
+
+#endif // INCLUDED_MEEVAX_CORE_STRING_HPP
+
