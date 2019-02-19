b7c71a3d381a2df7666efb9de9c5caaf90c3329d
yamacir-kit <httperror@404-notfound.jp>
2019-02-20 05:32:05 +0900

    Update unicode structure to support "is_character" concept

--- include/meevax/character/unicode.hpp
+++ include/meevax/character/unicode.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 
+#include <meevax/concepts/is_character.hpp>
+
 namespace meevax::character
 {
   template <auto N>
@@ -12,6 +14,8 @@ namespace meevax::character
   class unicode<8>
     : public std::string
   {};
+
+  DEFINE_CONCEPT_IS_CHARACTER_SPECIALIZATION(unicode<8>)
 } // namespace meevax::character
 
 #endif // INCLUDED_MEEVAX_CHARACTER_UNICODE_HPP
