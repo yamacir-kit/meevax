09aa4181c609b3f2d5ce4b8b2a85efb9ec7af70e
yamacir-kit <httperror@404-notfound.jp>
2019-02-24 03:49:10 +0900

    Move tokenization to header "string/tokenizer.hpp"

--- include/meevax/system/reader.hpp
+++ include/meevax/system/reader.hpp
@@ -1,15 +1,13 @@
 #ifndef INCLUDED_MEEVAX_SYSTEM_READER_HPP
 #define INCLUDED_MEEVAX_SYSTEM_READER_HPP
 
-#include <algorithm> // std::find_if
 #include <iostream>
 #include <iterator> // std::begin, std::end
-#include <limits>
-#include <list>
 #include <sstream>
 #include <string>
 #include <utility>
 
+#include <meevax/string/tokenizer.hpp>
 #include <meevax/system/cursor.hpp>
 #include <meevax/system/modular.hpp>
 
@@ -26,67 +24,7 @@ namespace meevax::system
 
     void operator()(std::istream& is)
     {
-      std::list<std::string> tokens {""};
-
-      for (auto c {static_cast<char>(is.get())}; c != EOF; c = is.get())
-      {
-        switch (c)
-        {
-        case ';':
-          is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-          break;
-
-        case '(':
-        case ')':
-          if (not tokens.back().empty())
-          {
-            tokens.emplace_back("");
-          }
-          tokens.back().push_back(c);
-          tokens.emplace_back("");
-          break;
-
-        case '\n':
-        case ' ':
-          if (not tokens.back().empty())
-          {
-            tokens.emplace_back("");
-          }
-          break;
-
-        case '"':
-          tokens.back().push_back(c);
-          tokens.back().push_back(is.get());
-
-          while (tokens.back().back() != '"')
-          {
-            tokens.back().push_back(is.get());
-          }
-          break;
-
-        default:
-          // std::cerr << "[debug] " << tokens.back() << " -> ";
-          tokens.back().push_back(c);
-          // std::cerr << tokens.back() << std::endl;
-        }
-      }
-
-      std::cerr << "[debug] tokens: ";
-      for (const auto& each : tokens)
-      {
-        switch (each[0])
-        {
-        case '"':
-          std::cerr << "\x1B[36m";
-          break;
-
-        default:
-          std::cerr << "\x1B[33m";
-        }
-
-        std::cerr << each << "\x1B[0m_";
-      }
-      std::cerr << std::endl;
+      string::tokenize(is);
     }
   };
 } // namespace meevax::system
