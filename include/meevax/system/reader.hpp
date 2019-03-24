5805beb387ab1d8fa0c17e213c05f856ffec73a5
yamacir-kit <httperror@404-notfound.jp>
2019-03-25 02:06:08 +0900

    Lipsticks

--- include/meevax/system/reader.hpp
+++ include/meevax/system/reader.hpp
@@ -5,10 +5,9 @@
 #include <iterator> // std::begin, std::end
 #include <limits> // std::numeric_limits<std::streamsize>
 #include <string>
-#include <utility>
+#include <utility> // std::forward
 
 #include <meevax/system/cursor.hpp>
-#include <meevax/system/exception.hpp>
 #include <meevax/system/modular.hpp>
 #include <meevax/system/number.hpp>
 #include <meevax/system/string.hpp>
@@ -27,32 +26,7 @@ namespace meevax::system
       , x002E {cursor::bind<std::string>("#\\x002E")}
     {}
 
-    template <typename CharType>
-    constexpr auto is_delimiter(CharType&& c) const noexcept
-    {
-      switch (c)
-      {
-      case u8'\x09': // '\t':
-      case u8'\x0A': // '\n':
-      case u8'\x0D': // '\r':
-      case u8'\x20': // ' ':
-      case u8'\x22': // '"':
-      case u8'\x23': // '#':
-      case u8'\x27': // '\'':
-      case u8'\x28': // '(':
-      case u8'\x29': // ')':
-      case u8'\x2C': // ',':
-      case u8'\x3B': // ';':
-      case u8'\x60': // '`':
-      case u8'\x7C': // '|':
-        return true;
-
-      default:
-        return false;
-      }
-    }
-
-    cursor operator()(std::istream& is) const
+    cursor read(std::istream& is) const
     {
       for (std::string buffer {is.narrow(is.get(), ' ')}; is; buffer.push_back(is.narrow(is.get(), ' '))) switch (buffer.back())
       {
@@ -65,20 +39,20 @@ namespace meevax::system
         break;
 
       case '(':
-        if (auto first {(*this)(is)}; first == x0020) // termination
+        if (auto first {read(is)}; first == x0020) // termination
         {
           return unit;
         }
         else if (first == x002E) // dot-notation
         {
-          auto second {(*this)(is)};
+          auto second {read(is)};
           is.ignore(std::numeric_limits<std::streamsize>::max(), ')');
           return second;
         }
         else
         {
           is.putback('(');
-          return cons(first, (*this)(is));
+          return cons(first, read(is));
         }
 
       case ')':
@@ -95,7 +69,7 @@ namespace meevax::system
           {
           case 'n':
             is.putback('"');
-            return cursor::bind<string>(cursor::bind<character>('\n'), (*this)(is));
+            return cursor::bind<string>(cursor::bind<character>('\n'), read(is));
 
           case '\n':
             while (std::isspace(is.peek()))
@@ -103,24 +77,24 @@ namespace meevax::system
               is.ignore(1);
             }
             is.putback('"');
-            return (*this)(is);
+            return read(is);
 
           case '"':
             is.putback('"');
-            return cursor::bind<string>(cursor::bind<character>("\""), (*this)(is));
+            return cursor::bind<string>(cursor::bind<character>("\""), read(is));
 
           default:
             is.putback('"');
-            return cursor::bind<string>(cursor::bind<character>("#\\unsupported;"), (*this)(is));
+            return cursor::bind<string>(cursor::bind<character>("#\\unsupported;"), read(is));
           }
 
         default:
           is.putback('"');
-          return cursor::bind<string>(cursor::bind<character>(c), (*this)(is));
+          return cursor::bind<string>(cursor::bind<character>(c), read(is));
         }
 
       case '\'':
-        return list(module.as<modular>().intern("quote"), (*this)(is));
+        return list(module.as<modular>().intern("quote"), read(is));
 
       case '#':
         return expand(is);
@@ -146,16 +120,48 @@ namespace meevax::system
       return cursor::bind<character>("end-of-file");
     }
 
+    template <typename... Ts>
+    decltype(auto) operator()(Ts&&... args) const
+    {
+      return read(std::forward<Ts>(args)...);
+    }
+
+  protected:
+    template <typename CharType>
+    constexpr bool is_delimiter(CharType&& c) const noexcept
+    {
+      switch (c)
+      {
+      case u8'\x09': // '\t':
+      case u8'\x0A': // '\n':
+      case u8'\x0D': // '\r':
+      case u8'\x20': // ' ':
+      case u8'\x22': // '"':
+      case u8'\x23': // '#':
+      case u8'\x27': // '\'':
+      case u8'\x28': // '(':
+      case u8'\x29': // ')':
+      case u8'\x2C': // ',':
+      case u8'\x3B': // ';':
+      case u8'\x60': // '`':
+      case u8'\x7C': // '|':
+        return true;
+
+      default:
+        return false;
+      }
+    }
+
     cursor expand(std::istream& is) const
     {
       switch (is.peek())
       {
       case 't':
-        (*this)(is); // XXX DIRTY HACK (IGNORE FOLLOWING CHARACTERS)
+        read(is); // XXX DIRTY HACK (IGNORE FOLLOWING CHARACTERS)
         return true_v;
 
       case 'f':
-        (*this)(is); // XXX DIRTY HACK (IGNORE FOLLOWING CHARACTERS)
+        read(is); // XXX DIRTY HACK (IGNORE FOLLOWING CHARACTERS)
         return false_v;
 
       default:
