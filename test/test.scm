4809069d7ba9570abfe109c13796171161796f6c
yamacir-kit <httperror@404-notfound.jp>
2019-05-21 16:38:33 +0900

    Fix so many bug
    assocとunsafe_assocを統合しようとした時に、中途半端に放棄したのが原因だった。
    あと、幾つかのプリミティブがアンバウンド扱いになるのがずっと謎だったが、
    組み込みプロシージャを動的ロードする形に変更したことを忘れていたのが原因。
    つまり、setup.scm無しでは満足に動かなくなっているのを失念してた。

--- test/test.scm
+++ test/test.scm
@@ -1,7 +1,24 @@
 (load "../setup.scm")
 
-(define hoge 42)
-(display "hoge is " hoge)
+(define x 42)
+x
+(set! x 100)
+x
+
+(define y 'hoge)
+y
+(set! y 100)
+y
+
+(define accumulator
+  (lambda (n)
+    (lambda ()
+      (set! n (+ n 1)))))
+
+(define acc (accumulator x))
+(acc)
+(acc)
+(acc)
 
 (emergency-exit)
 
