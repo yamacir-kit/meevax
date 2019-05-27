fe0ea5369a0b89277a1c61bd6b496ee29af7eb64
yamacir-kit <httperror@404-notfound.jp>
2019-05-27 22:46:31 +0900

    Add macro "and", "or", and rewrite "quasiquote"
    Reference:
      "Quasiquotation in Lisp" Appendix B

--- setup.scm
+++ setup.scm
@@ -22,39 +22,38 @@
   (lambda (e)
     (cdr e)))
 
-
 ;; cxr
 
 (define caar (lambda (e) (car (car e))))
 (define cadr (lambda (e) (car (cdr e))))
 (define cdar (lambda (e) (cdr (car e))))
-(define caar (lambda (e) (car (car e))))
+(define cddr (lambda (e) (cdr (cdr e))))
 
-(define caaar (lambda (e) (car (car (car e)))))
-(define caadr (lambda (e) (car (car (cdr e)))))
-(define cadar (lambda (e) (car (cdr (car e)))))
-(define caddr (lambda (e) (car (cdr (cdr e)))))
-(define cdaar (lambda (e) (cdr (car (car e)))))
-(define cdadr (lambda (e) (cdr (car (cdr e)))))
-(define cddar (lambda (e) (cdr (cdr (car e)))))
-(define cdddr (lambda (e) (cdr (cdr (cdr e)))))
-
-(define caaaar (lambda (e) (car (car (car (car e))))))
-(define caaadr (lambda (e) (car (car (car (cdr e))))))
-(define caadar (lambda (e) (car (car (cdr (car e))))))
-(define caaddr (lambda (e) (car (car (cdr (cdr e))))))
-(define cadaar (lambda (e) (car (cdr (car (car e))))))
-(define cadadr (lambda (e) (car (cdr (car (cdr e))))))
-(define caddar (lambda (e) (car (cdr (cdr (car e))))))
-(define cadddr (lambda (e) (car (cdr (cdr (cdr e))))))
-(define cdaaar (lambda (e) (cdr (car (car (car e))))))
-(define cdaadr (lambda (e) (cdr (car (car (cdr e))))))
-(define cdadar (lambda (e) (cdr (car (cdr (car e))))))
-(define cdaddr (lambda (e) (cdr (car (cdr (cdr e))))))
-(define cddaar (lambda (e) (cdr (cdr (car (car e))))))
-(define cddadr (lambda (e) (cdr (cdr (car (cdr e))))))
-(define cdddar (lambda (e) (cdr (cdr (cdr (car e))))))
-(define cddddr (lambda (e) (cdr (cdr (cdr (cdr e))))))
+; (define caaar (lambda (e) (car (car (car e)))))
+; (define caadr (lambda (e) (car (car (cdr e)))))
+; (define cadar (lambda (e) (car (cdr (car e)))))
+; (define caddr (lambda (e) (car (cdr (cdr e)))))
+; (define cdaar (lambda (e) (cdr (car (car e)))))
+; (define cdadr (lambda (e) (cdr (car (cdr e)))))
+; (define cddar (lambda (e) (cdr (cdr (car e)))))
+; (define cdddr (lambda (e) (cdr (cdr (cdr e)))))
+;
+; (define caaaar (lambda (e) (car (car (car (car e))))))
+; (define caaadr (lambda (e) (car (car (car (cdr e))))))
+; (define caadar (lambda (e) (car (car (cdr (car e))))))
+; (define caaddr (lambda (e) (car (car (cdr (cdr e))))))
+; (define cadaar (lambda (e) (car (cdr (car (car e))))))
+; (define cadadr (lambda (e) (car (cdr (car (cdr e))))))
+; (define caddar (lambda (e) (car (cdr (cdr (car e))))))
+; (define cadddr (lambda (e) (car (cdr (cdr (cdr e))))))
+; (define cdaaar (lambda (e) (cdr (car (car (car e))))))
+; (define cdaadr (lambda (e) (cdr (car (car (cdr e))))))
+; (define cdadar (lambda (e) (cdr (car (cdr (car e))))))
+; (define cdaddr (lambda (e) (cdr (car (cdr (cdr e))))))
+; (define cddaar (lambda (e) (cdr (cdr (car (car e))))))
+; (define cddadr (lambda (e) (cdr (cdr (car (cdr e))))))
+; (define cdddar (lambda (e) (cdr (cdr (cdr (car e))))))
+; (define cddddr (lambda (e) (cdr (cdr (cdr (cdr e))))))
 
 (define list
   (lambda xs xs))
@@ -63,10 +62,6 @@
   (lambda (e)
     (eq? e '())))
 
-(define not
-  (lambda (e)
-    (if e #false #true)))
-
 (define append
   (lambda (xs ys)
     (if (null? xs)
@@ -74,22 +69,93 @@
         (cons (car xs)
               (append (cdr xs) ys)))))
 
-(define transform
+(define and
+  (macro e
+    (if (null? e) #true
+    (if (null? (cdr e)) #true
+    (if (null? (cddr e))
+        (cadr e)
+        (list (list 'lambda (list head thunk)
+                (list 'if 'head
+                          (list 'thunk)
+                          'head))
+              (cadr e)
+              (list 'lambda '()
+                (append (list 'and)
+                        (cddr e)))))))))
+
+(define or
+  (macro e
+    (if (null? e) #false
+    (if (null? (cdr e)) #false
+    (if (null? (cddr e))
+        (cadr e)
+        (list (list 'lambda (list head thunk)
+                (list 'if 'head
+                          'head
+                          (list 'thunk)))
+              (cadr e)
+              (list 'lambda '()
+                (append (list 'or)
+                        (cddr e)))))))))
+
+(define not
   (lambda (e)
-    (if (not (pair? e))
-        (list 'quote e)
-        (if (not (pair? (car e)))
-            (list 'cons (list 'quote (car e)) (transform (cdr e)))
-            (if (eq? (caar e) 'unquote)
-                (list 'cons (cadar e) (transform (cdr e)))
-                (if (eq? (caar e) 'unquote-splicing)
-                    (list 'append (cadar e) (transform (cdr e)))
-                    (list 'cons (transform (car e))
-                                (transform (cdr e)))))))))
+    (if e #false #true)))
+
+(define quasiquote-expand
+  (lambda (e depth)
+    (if (not (pair? e)) `',e
+        (if (eq? (car e) 'quasiquote)
+            `(cons 'quasiquote ,(quasiquote-expand (cdr e) (+ depth 1)))
+            (if (or (eq? (car e) 'unquote)
+                    (eq? (car e) 'unquote-splicing))
+                (if (< 0 depth)
+                    `(cons ',(car e) ,(quasiquote-expand (cdr e) (- depth 1)))
+                    (if (and (eq? (car e) 'unquote)
+                             (not (null? (cdr e)))
+                             (null? (cddr e)))
+                        (cadr e)
+                        (error "illegal")))
+                `(append ,(quasiquote-expand-list (car e) depth)
+                         ,(quasiquote-expand      (cdr e) depth)))))))
+
+(define quasiquote-expand-list
+  (lambda (e depth)
+    (if (not (pair? e)) `'(,e)
+        (if (eq? (car e) 'quasiquote)
+            `(list (cons 'quasiquote)
+                   ,(quasiquote-expand (cdr e) (+ depth 1)))
+            (if (or (eq? (car e) 'unquote)
+                    (eq? (car e) 'unquote-splicing))
+                (if (< 0 depth)
+                    `(list ',(car e) ,(quasiquote-expand (cdr e) (- depth 1)))
+                    (if (eq? (car e) 'unquote)
+                        `(list . ,(cdr e))
+                        `(append . ,(cdr e))))
+                `(list (append ,(quasiquote-expand-list (car e) depth)
+                               ,(quasiquote-expand      (cdr e) depth))))))))
 
 (define quasiquote
-  (macro (e)
-    (transform e)))
+  (macro e
+    (quasiquote-expand e 0)))
+
+; (define qq-expand
+;   (lambda (e)
+;     (if (not (pair? e))
+;         (list 'quote e)
+;         (if (not (pair? (car e)))
+;             (list 'cons (list 'quote (car e)) (qq-expand (cdr e)))
+;             (if (eq? (caar e) 'unquote)
+;                 (list 'cons (cadar e) (qq-expand (cdr e)))
+;                 (if (eq? (caar e) 'unquote-splicing)
+;                     (list 'append (cadar e) (qq-expand (cdr e)))
+;                     (list 'cons (qq-expand (car e))
+;                                 (qq-expand (cdr e)))))))))
+;
+; (define quasiquote
+;   (macro (e)
+;     (qq-expand e)))
 
 (define current-lexical-environment
   (macro ()
@@ -102,4 +168,8 @@
 (define rename
   (lambda (x)
     (lambda () x)))
+
+(define apply
+  (lambda (proc args)
+    (proc . args)))
 
--- test/test.scm
+++ test/test.scm
@@ -1,56 +1,69 @@
 (load "../setup.scm")
 
-(define x 42)
-x
-(set! x 100)
-x
+; (define x 42)
+; x
+; (set! x 100)
+; x
+;
+; (define y 'hoge)
+; y
+; (set! y 100)
+; y
+;
+; (define accumulator
+;   (lambda (n)
+;     (lambda ()
+;       (set! n (+ n 1)))))
+;
+; (define acc (accumulator x))
+; (acc)
+; (acc)
+; (acc)
 
-(define y 'hoge)
-y
-(set! y 100)
-y
+; (define or-expand
+;   (lambda args
+;     (if (null? (car args))
+;         #false
+;         (list (list 'lambda (list 'result)
+;                 (list 'if 'result
+;                           'result
+;                         (or-expand (cdar args))))
+;          (caar args)))))
+;
+; (define or
+;   (macro e
+;     (or-expand e)))
 
-(define accumulator
-  (lambda (n)
-    (lambda ()
-      (set! n (+ n 1)))))
-
-(define acc (accumulator x))
-(acc)
-(acc)
-(acc)
+(or)
+(or #true)
+(or #false)
+(or (eq? 'a 'a) (eq? 'b 'b))
+(or (eq? 'a 'b) (eq? 'c 'c))
+(or (eq? 'a 'b) (eq? 'c 'd))
 
 (define a '(1 2 3))
-(transform '(a b c)); => (cons 'a (cons 'b (cons 'c '())))
-(transform '(,a b c)); => (cons a (cons 'b (cons 'c '())))
-(transform '(,a ,@b c)); => (cons a (append b (cons 'c '())))
-(transform '(,(car a) ,@(cdr b) c)); => (cons (car a) (append (cdr b) (cons 'c '())))
+(quasiquote-expand '(a b c)); => (cons 'a (cons 'b (cons 'c '())))
+(quasiquote-expand '(,a b c)); => (cons a (cons 'b (cons 'c '())))
+(quasiquote-expand '(,a ,@b c)); => (cons a (append b (cons 'c '())))
+(quasiquote-expand '(,(car a) ,@(cdr b) c)); => (cons (car a) (append (cdr b) (cons 'c '())))
 
 `(a b c); => (a b c)
 `(,a b c); => ((1 2 3) b c)
 `(,@a b c); => (1 2 3 b c)
 
-(define map
-  (lambda (callee &list)
-    (if (null? &list)
-        '()
-        (cons (callee (car &list))
-              (map callee (cdr &list))))))
-
-(define let
-  (macro (bindings . body)
-    `((lambda ,(map car bindings) ,@body) ,@(map cadr bindings))
-    ; ((append (list 'lambda (map car bindings)) body)
-    ;  (map cadr bindings))
-))
-
-(let ((a 1)
-      (b 2))
-  (+ a b)
-  (* a b))
-
-; ((lambda (x y)
-;    (+ x y))
-;  1 2)
-
+; (define map
+;   (lambda (callee &list)
+;     (if (null? &list)
+;         '()
+;         (cons (callee (car &list))
+;               (map callee (cdr &list))))))
+;
+; (define let
+;   (macro (bindings . body)
+;     `((lambda ,(map car bindings) ,@body)
+;       ,@(map cadr bindings))))
+;
+; (let ((a 1)
+;       (b 2))
+;   (+ a b))
 
