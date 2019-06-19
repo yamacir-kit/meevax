0ed8b43f949ab9ab267e119722e5bc5510c9f3f0
yamacir-kit <httperror@404-notfound.jp>
2019-06-19 18:48:45 +0900

    Move test macro definition to "expect.scm"

--- test/test.scm
+++ test/test.scm
@@ -1,22 +1,5 @@
 (load "../setup.scm")
-
-(define passed 0)
-
-(define expect
-  (macro (expects expression)
-   `(let ((result ,expression))
-      (if (equal? result ',expects)
-          (begin (set! passed (+ passed 1))
-                 result)
-          (begin (display "; test          ; expected ") ; TODO SUPPORT TAB
-                 (display ',expects)
-                 (display " as result of ")
-                 (display ',expression)
-                 (display ", but got ")
-                 (display result)
-                 (newline)
-                 (emergency-exit))))))
-
+(load "../test/expect.scm")
 
 ; ------------------------------------------------------------------------------
 ;   4.1.1 Variable References
