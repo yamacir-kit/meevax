5cdb97d26fdf0426b59fcdb2519f763ee026995d
yamacir-kit <httperror@404-notfound.jp>
2019-06-27 22:55:24 +0900

    Add cairo context wrapper

---
+++ include/meevax/cairo/context.hpp
@@ -0,0 +1,40 @@
+#ifndef INCLUDED_MEEVAX_CAIRO_SURFACE_HPP
+#define INCLUDED_MEEVAX_CAIRO_SURFACE_HPP
+
+#include <memory>
+#include <utility>
+
+#include <cairo/cairo.h>
+
+#include <meevax/cairo/surface.hpp>
+
+namespace meevax::cairo
+{
+  struct context
+    : public std::shared_ptr<cairo_t>
+  {
+    explicit context(const surface& surface)
+      : std::shared_ptr<cairo_t> {cairo_create(surface), cairo_destroy}
+    {}
+
+    operator element_type*() const noexcept
+    {
+      return get();
+    }
+
+    #define CAIRO_SIMPLE_CONTEXT_OPERATOR(NAME)                                \
+    template <typename... Ts>                                                  \
+    decltype(auto) NAME(Ts&&... xs) noexcept                                   \
+    {                                                                          \
+      return cairo_##NAME(*this, std::forward<Ts>(xs)...);                     \
+    }
+
+    CAIRO_SIMPLE_CONTEXT_OPERATOR(set_source_rgb)
+    CAIRO_SIMPLE_CONTEXT_OPERATOR(paint)
+
+    #undef CAIRO_SIMPLE_CONTEXT_OPERATOR
+  };
+} // namespace meevax::cairo
+
+#endif // INCLUDED_MEEVAX_CAIRO_SURFACE_HPP
+
