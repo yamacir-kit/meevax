379bbd709c4987eda8aa9e40eacb118e537f562c
yamacir-kit <httperror@404-notfound.jp>
2019-07-24 20:55:01 +0900

    Update configuration values as runtime object

--- configure/configure.hpp.cmake
+++ configure/configure.hpp.cmake
@@ -1,14 +1,16 @@
 #ifndef INCLUDED_MEEVAX_CONFIGURE_HPP
 #define INCLUDED_MEEVAX_CONFIGURE_HPP
 
-#include <string>
+#include <meevax/system/numerical.hpp>
+#include <meevax/system/srfi-1.hpp>
 
 namespace meevax
 {
-  static const std::string version_major {"${PROJECT_VERSION_MAJOR}"};
-  static const std::string version_minor {"${PROJECT_VERSION_MINOR}"};
-  static const std::string version_patch {"${PROJECT_VERSION_PATCH}"};
-  static const std::string version       {"${PROJECT_VERSION}"};
+  static const auto version_major {system::make<system::real>(${PROJECT_VERSION_MAJOR})};
+  static const auto version_minor {system::make<system::real>(${PROJECT_VERSION_MINOR})};
+  static const auto version_patch {system::make<system::real>(${PROJECT_VERSION_PATCH})};
+
+  static const auto version {system::list(version_major, version_minor, version_patch)};
 
   static const std::string build_date {"${${PROJECT_NAME}_BUILD_DATE}"};
   static const std::string build_hash {"${${PROJECT_NAME}_BUILD_HASH}"};
