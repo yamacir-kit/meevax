2355f4b15961fdd25653431318db8badf8e12bb3
yamacir-kit <httperror@404-notfound.jp>
2019-08-11 06:21:28 +0900

    Revert "Update configure to dependent from meevax builtin-types"
    This reverts commit 5fd7a526c7333c89ccdebd971372b69aa283dfa5.

--- configure/configure.hpp.cmake
+++ configure/configure.hpp.cmake
@@ -3,21 +3,29 @@
 
 #include <string>
 
+#include <meevax/system/numerical.hpp>
+#include <meevax/system/path.hpp>
+#include <meevax/system/srfi-1.hpp>
+
 namespace meevax::system
 {
   struct configure
   {
-    static inline const std::string version_major {"${PROJECT_VERSION_MAJOR}"};
-    static inline const std::string version_minor {"${PROJECT_VERSION_MINOR}"};
-    static inline const std::string version_patch {"${PROJECT_VERSION_PATCH}"};
+    static inline const auto version_major {make<real>(${PROJECT_VERSION_MAJOR})};
+    static inline const auto version_minor {make<real>(${PROJECT_VERSION_MINOR})};
+    static inline const auto version_patch {make<real>(${PROJECT_VERSION_PATCH})};
 
-    static inline const std::string version {"${PROJECT_VERSION}"};
+    static inline const auto version {list(
+      version_major, version_minor, version_patch
+    )};
 
     static inline const std::string build_date {"${${PROJECT_NAME}_BUILD_DATE}"};
     static inline const std::string build_hash {"${${PROJECT_NAME}_BUILD_HASH}"};
     static inline const std::string build_type {"${CMAKE_BUILD_TYPE}"};
 
-    static inline const std::string install_prefix {"${CMAKE_INSTALL_PREFIX}"};
+    static inline const auto install_prefix {make<path>(
+      "${CMAKE_INSTALL_PREFIX}"
+    )};
   };
 } // namespace meevax
 
