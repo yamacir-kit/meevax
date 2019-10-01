b1216d67952bd089fc5c9e0c11b5833403761963
yamacir-kit <httperror@404-notfound.jp>
2019-10-01 17:45:57 +0900

    Add character utilities

--- library/character.cpp
+++ library/character.cpp
@@ -1,11 +1,38 @@
-#include <meevax/kernel/native.hpp>
 #include <meevax/kernel/character.hpp>
+#include <meevax/kernel/native.hpp>
+#include <meevax/kernel/numerical.hpp>
 
 extern "C" namespace meevax::string
 {
   NATIVE(is_character)
   {
     return kernel::car(operands).is<kernel::character>() ? kernel::true_object : kernel::false_object;
+  }
+
+  NATIVE(digit_value)
+  {
+    // XXX INCORRECT!!!
+    return kernel::make<kernel::real>(
+             kernel::car(operands).as<const std::string>()
+           );
+  }
+
+  NATIVE(character_to_real)
+  {
+    switch (const std::string& s {
+              kernel::car(operands).as<const std::string&>()
+            }; s.size())
+    {
+    case 1:
+      return kernel::make<kernel::real>(
+               *reinterpret_cast<const std::uint8_t*>(s.data())
+             );
+
+    default:
+      throw kernel::make<kernel::evaluation_error>(
+              "character_to_real - unsupported"
+            );
+    }
   }
 } // extern "C"
 
