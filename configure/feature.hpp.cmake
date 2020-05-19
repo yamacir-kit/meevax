ec220051c9127c939cf33ae6e343f505f94e00af
yamacir-kit <httperror@404-notfound.jp>
2020-05-20 02:35:30 +0900

    Update feature to lower case

--- configure/feature.hpp.cmake
+++ configure/feature.hpp.cmake
@@ -1,7 +1,10 @@
 #ifndef INCLUDED_MEEVAX_KERNEL_FEATURE_HPP
 #define INCLUDED_MEEVAX_KERNEL_FEATURE_HPP
 
+#include <boost/algorithm/string.hpp>
+
 #include <meevax/kernel/list.hpp>
+#include <meevax/kernel/numerical.hpp>
 #include <meevax/kernel/symbol.hpp>
 
 namespace meevax::kernel
@@ -9,14 +12,18 @@ namespace meevax::kernel
   struct feature
     : public object
   {
+    static inline const std::string build_type_origin { "${CMAKE_BUILD_TYPE}" };
+
     static inline const auto build_date { make<symbol>("${${PROJECT_NAME}_BUILD_DATE}") };
     static inline const auto build_hash { make<symbol>("${${PROJECT_NAME}_BUILD_HASH}") };
-    static inline const auto build_type { make<symbol>("${CMAKE_BUILD_TYPE}") };
+    static inline const auto build_type { make<symbol>(boost::to_lower_copy(build_type_origin)) };
 
-    static inline const auto compiler { make<symbol>("${CMAKE_CXX_COMPILER}") };
-    static inline const auto flags    { make<symbol>("${CMAKE_CXX_FLAGS}") };
+    static inline const auto cxx_compiler { make<symbol>("${CMAKE_CXX_COMPILER}") };
+    static inline const auto cxx_flags    { make<symbol>("${CMAKE_CXX_FLAGS}") };
+    static inline const auto cxx_standard { make<real>("${CMAKE_CXX_STANDARD}") };
 
-    static inline const auto system_name { make<symbol>("${CMAKE_SYSTEM_NAME}") };
+    static inline const std::string system_name_origin { "${CMAKE_SYSTEM_NAME}" };
+    static inline const auto system_name { make<symbol>(boost::to_lower_copy(system_name_origin)) };
 
     static inline const auto implementation_name              { make<symbol>("${PROJECT_NAME}") };
     static inline const auto implementation_name_with_version { make<symbol>("${PROJECT_NAME}-${PROJECT_VERSION}") };
