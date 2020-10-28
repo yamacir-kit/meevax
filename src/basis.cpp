7fb7c72bc9e413316406e6b6fa6ab9d8e4252fdf
yamacir-kit <httperror@404-notfound.jp>
2020-10-28 22:56:40 +0900

    Lipsticks

--- src/basis.cpp
+++ src/basis.cpp
@@ -9,14 +9,11 @@
  *
  * -------------------------------------------------------------------------- */
 
-extern char _binary_overture_ss_start;
-extern char _binary_overture_ss_end;
+extern const char _binary_overture_ss_start;
+extern const char _binary_overture_ss_end;
+extern const char _binary_overture_ss_size;
 
 namespace meevax { inline namespace kernel
 {
-  const string_view overture
-  {
-                                                        &_binary_overture_ss_start,
-    static_cast<std::size_t>(&_binary_overture_ss_end - &_binary_overture_ss_start)
-  };
+  const string_view overture { &_binary_overture_ss_start, reinterpret_cast<std::size_t>(&_binary_overture_ss_size) };
 }} // namespace meevax::kernel
