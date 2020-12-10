e7a2a949d2ce05930933e301f22b9e40399c708b
yamacir-kit <httperror@404-notfound.jp>
2020-12-11 01:51:15 +0900

    Update option '--help'

--- include/meevax/kernel/configurator.hpp
+++ include/meevax/kernel/configurator.hpp
@@ -59,56 +59,25 @@ namespace meevax { inline namespace kernel
     let display_version() const
     {
       write_line("Meevax Lisp System, version ", current_version.semantic());
-      newline();
-      write(
-        "  Copyright (C) 2020 Tatsuya Yamasaki\n"
-        "\n"
-        "  Licensed under the Apache License, Version 2.0 (the \"License\");\n"
-        "  you may not use this file except in compliance with the License.\n"
-        "  You may obtain a copy of the License at\n"
-        "\n"
-        "      http://www.apache.org/licenses/LICENSE-2.0\n"
-        "\n"
-        "  Unless required by applicable law or agreed to in writing, software\n"
-        "  distributed under the License is distributed on an \"AS IS\" BASIS,\n"
-        "  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
-        "  See the License for the specific language governing permissions and\n"
-        "  limitations under the License.\n");
-
       return unspecified;
     }
 
-    auto display_version_() const -> const auto&
+    let display_license() const
     {
-      write( //  10        20        30        40        50        60        70        80\n"
-        "; Meevax Lisp System ", current_version.major(), " - Revision ", current_version.minor(), " Patch ", current_version.patch(), "\n"
-        "; ------------------------------------------------------------------------------\n"
-        "; version               ; ", current_version.semantic(),                       "\n"
-        );
-
-      write_to(standard_verbose_port(),
-        "; license               ; ", unspecified,                                      "\n"
-        ";\n"
-        "; build-date            ; ", current_feature.build_date(),                     "\n"
-        "; build-hash            ; ", current_feature.build_hash(),                     "\n"
-        "; build-type            ; ", current_feature.build_type(),                     "\n"
-        ";\n"
-        "; cxx-compiler          ; ", current_feature.cxx_compiler(),                   "\n"
-        "; cxx-flags             ; ", current_feature.cxx_flags(),                      "\n"
-        "; cxx-standard          ; ", current_feature.cxx_standard(),                   "\n"
-        ";\n"
-        "; system-processor      ; ", current_feature.system_processor(),               "\n"
-        "; system-name           ; ", current_feature.system_name(),                    "\n"
-        ";\n"
-        "; install-prefix        ; ", current_feature.install_prefix(),                 "\n"
-        ";\n"
-        "; libraries             ; ", current_version.libraries(),                      "\n"
-        ";\n"
-        );
-
       write(
-        "; feature               ; ", current_feature, "\n"
-        );
+        "Copyright (C) 2020 Tatsuya Yamasaki\n"
+        "\n"
+        "Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+        "you may not use this file except in compliance with the License.\n"
+        "You may obtain a copy of the License at\n"
+        "\n"
+        "    http://www.apache.org/licenses/LICENSE-2.0\n"
+        "\n"
+        "Unless required by applicable law or agreed to in writing, software\n"
+        "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+        "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+        "See the License for the specific language governing permissions and\n"
+        "limitations under the License.\n");
 
       return unspecified;
     }
@@ -189,6 +158,8 @@ namespace meevax { inline namespace kernel
       std::make_pair('v', [this](auto&&...)
       {
         display_version();
+        newline();
+        display_license();
         return std::exit(boost::exit_success), unspecified;
       }),
     };
