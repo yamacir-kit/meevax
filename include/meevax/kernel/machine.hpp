540a6c08f54bd9736f7befcbbfb052a116a485ad
yamacir-kit <httperror@404-notfound.jp>
2021-01-30 05:29:31 +0900

    Lipsticks

--- include/meevax/kernel/machine.hpp
+++ include/meevax/kernel/machine.hpp
@@ -71,7 +71,7 @@ inline namespace kernel
      * ---------------------------------------------------------------------- */
     let const global(let const& x, let & g)
     {
-      if (let const binding = assq(x, g); eq(binding, f) /* or cdr(binding).is<keyword>() */)
+      if (let const binding = assq(x, g); eq(binding, f) /* or cdr(binding).is<keyword>() */) // TODO
       {
         /* ---- R7RS 5.3.1. Top level definitions ------------------------------
          *
@@ -98,7 +98,7 @@ inline namespace kernel
       }
     }
 
-    /* ---- Compiler -----------------------------------------------------------
+    /* ---- R7RS 4. Expressions ------------------------------------------------
      *
      *  <expression> = <identifier>
      *               | <literal>
@@ -107,6 +107,16 @@ inline namespace kernel
      *               | <conditional>
      *               | <assignment>
      *               | <derived expression>
+     *
+     *  Expression types are categorized as primitive or derived. Primitive
+     *  expression types include variables and procedure calls. Derived
+     *  expression types are not semantically primitive, but can instead be
+     *  defined as macros. Suitable syntax definitions of some of the derived
+     *  expressions are given in section 7.3.
+     *
+     *  The procedures force, promise?, make-promise, and make-parameter are
+     *  also described in this chapter because they are intimately associated
+     *  with the delay, delay-force, and parameterize expression types.
      *
      * ---------------------------------------------------------------------- */
     let compile(
@@ -118,12 +128,30 @@ inline namespace kernel
     {
       if (expression.is<null>())
       {
+        /* ---- R7RS 4.1.3. Procedure calls ------------------------------------
+         *
+         *  (<operator> <operand 1> ...)                                 syntax
+         *
+         *  Note: In many dialects of Lisp, the empty list, (), is a legitimate
+         *  expression evaluating to itself. In Scheme, it is an error.
+         *
+         * ------------------------------------------------------------------ */
         return cons(make<instruction>(mnemonic::LOAD_CONSTANT), unit, continuation);
       }
       else if (not expression.is<pair>()) // is <identifier>
       {
         if (is_identifier(expression))
         {
+          /* ---- R7RS 4.1.1. Variable references ------------------------------
+           *
+           *  <variable>                                                 syntax
+           *
+           *  An expression consisting of a variable (section 3.1) is a
+           *  variable reference. The value of the variable reference is the
+           *  value stored in the location to which the variable is bound. It
+           *  is an error to reference an unbound variable.
+           *
+           * ------------------------------------------------------------------ */
           if (de_bruijn_index index { expression, frames }; index)
           {
             if (index.is_variadic())
@@ -186,6 +214,45 @@ inline namespace kernel
             return compile(in_context_free, expanded, syntactic_environment, frames, continuation);
           }
         }
+
+        /* ---- R7RS 4.1.3. Procedure calls ------------------------------------
+         *
+         *  (<operator> <operand 1> ...)                                 syntax
+         *
+         *  A procedure call is written by enclosing in parentheses an
+         *  expression for the procedure to be called followed by expressions
+         *  for the arguments to be passed to it. The operator and operand
+         *  expressions are evaluated (in an unspecified order) and the
+         *  resulting procedure is passed the resulting arguments.
+         *
+         *  The procedures in this document are available as the values of
+         *  variables exported by the standard libraries. For example, the
+         *  addition and multiplication procedures in the above examples are
+         *  the values of the variables + and * in the base library. New
+         *  procedures are created by evaluating lambda expressions (see
+         *  section 4.1.4).
+         *
+         *  Procedure calls can return any number of values (see values in
+         *  section 6.10). Most of the procedures defined in this report return
+         *  one value or, for procedures such as apply, pass on the values
+         *  returned by a call to one of their arguments. Exceptions are noted
+         *  in the individual descriptions.
+         *
+         *  Note: In contrast to other dialects of Lisp, the order of
+         *  evaluation is unspecified, and the operator expression and the
+         *  operand expressions are always evaluated with the same evaluation
+         *  rules.
+         *
+         *  Note: Although the order of evaluation is otherwise unspecified,
+         *  the effect of any concurrent evaluation of the operator and operand
+         *  expressions is constrained to be consistent with some sequential
+         *  order of evaluation. The order of evaluation may be chosen
+         *  differently for each procedure call.
+         *
+         *  Note: In many dialects of Lisp, the empty list, (), is a legitimate
+         *  expression evaluating to itself. In Scheme, it is an error.
+         *
+         * ------------------------------------------------------------------ */
 
         debug(magenta, "(", reset, faint, " ; is <procedure call>");
         indent() >> shift();
@@ -511,12 +578,26 @@ inline namespace kernel
     }
 
   protected: // Primitive Expression Types
-    /* ---- Quotation ----------------------------------------------------------
-     *
-     *  <quotation> = (quote <datum>)
-     *
-     * ---------------------------------------------------------------------- */
-    SYNTAX(quotation)
+    SYNTAX(quotation) /* -------------------------------------------------------
+    *
+    *  (quote <datum>)                                                   syntax
+    *
+    *  (quote <datum>) evaluates to <datum>. <Datum> can be any external
+    *  representation of a Scheme object (see section 3.3). This notation is
+    *  used to include literal constants in Scheme code.
+    *
+    *  (quote <datum>) can be abbreviated as '<datum>. The two notations are
+    *  equivalent in all respects.
+    *
+    *  Numerical constants, string constants, character constants, vector
+    *  constants, bytevector constants, and boolean constants evaluate to
+    *  themselves; they need not be quoted.
+    *
+    *  As noted in section 3.4, it is an error to attempt to alter a constant
+    *  (i.e. the value of a literal expression) using a mutation procedure like
+    *  set-car! or string-set!.
+    *
+    * ----------------------------------------------------------------------- */
     {
       debug(car(expression), faint, " ; is <datum>");
       return cons(make<instruction>(mnemonic::LOAD_CONSTANT), car(expression), continuation);
