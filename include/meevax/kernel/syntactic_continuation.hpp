d536bdb0349ddcb8898568a93ab33897aa921e2b
yamacir-kit <httperror@404-notfound.jp>
2021-03-02 00:52:16 +0900

    Update symbol table to be unique globally

--- include/meevax/kernel/syntactic_continuation.hpp
+++ include/meevax/kernel/syntactic_continuation.hpp
@@ -71,7 +71,8 @@ inline namespace kernel
     * ----------------------------------------------------------------------- */
   {
   public:
-    std::unordered_map<std::string, object> symbols;
+    static inline std::unordered_map<std::string, object> symbols;
+
     std::unordered_map<std::string, object> external_symbols; // TODO REMOVE
 
     std::size_t generation = 0;
