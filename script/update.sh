a0d7f5577762b09bc93cd97d7c999a931cfb63c9
yamacir-kit <httperror@404-notfound.jp>
2023-04-16 14:59:42 +0900

    Merge scripts `install.sh` and `version.sh` into `update.sh`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

---
+++ script/update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# ./script/update.sh -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++
+
+root="$(git rev-parse --show-toplevel)"
+
+build()
+{
+  rm -rf "$1/build"
+  cmake -B "$1/build" -S "$@"
+  cmake --build "$1/build" --target develop
+}
+
+echo "0.4.$(($(git rev-list --no-merges --count HEAD) - 2854))" > "$root/VERSION"
+
+if dpkg -s meevax
+then
+  sudo apt remove --yes meevax
+fi
+
+build "$root" "$@"
+
+build "$root/example" "$@"
