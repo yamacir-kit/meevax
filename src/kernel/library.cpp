6dfb5582eb4569fc0c87b81bd1ea70bc1540cff2
yamacir-kit <httperror@404-notfound.jp>
2023-05-21 12:51:36 +0900

    Update to use array subscript operator of type `object` in `boot()`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- src/kernel/library.cpp
+++ src/kernel/library.cpp
@@ -95,12 +95,12 @@ inline namespace kernel
     {
       library.define<procedure>("char?", [](let const& xs)
       {
-        return car(xs).is<character>();
+        return xs[0].is<character>();
       });
 
       library.define<procedure>("digit-value", [](let const& xs)
       {
-        if (auto c = car(xs).as<character>(); std::isdigit(c.codepoint))
+        if (auto c = xs[0].as<character>(); std::isdigit(c.codepoint))
         {
           return make<exact_integer>(c.codepoint - '0');
         }
@@ -112,7 +112,7 @@ inline namespace kernel
 
       library.define<procedure>("integer->char", [](let const& xs)
       {
-        return make<character>(car(xs).as<exact_integer>());
+        return make<character>(xs[0].as<exact_integer>());
       });
     });
 
@@ -128,12 +128,12 @@ inline namespace kernel
 
       library.define<procedure>("real-part", [](let const& xs)
       {
-        return car(xs).as<complex>().real();
+        return xs[0].as<complex>().real();
       });
 
       library.define<procedure>("imag-part", [](let const& xs)
       {
-        return car(xs).as<complex>().imag();
+        return xs[0].as<complex>().imag();
       });
     });
 
@@ -141,7 +141,7 @@ inline namespace kernel
     {
       library.define<procedure>("emergency-exit", [](let const& xs)
       {
-        if (let const& status = car(xs); status.is<null>())
+        if (let const& status = xs[0]; status.is<null>())
         {
           throw success;
         }
@@ -172,12 +172,12 @@ inline namespace kernel
     {
       library.define<procedure>("eq?", [](let const& xs)
       {
-        return eq(car(xs), cadr(xs));
+        return eq(xs[0], xs[1]);
       });
 
       library.define<procedure>("eqv?", [](let const& xs)
       {
-        return eqv(car(xs), cadr(xs));
+        return eqv(xs[0], xs[1]);
       });
     });
 
@@ -217,7 +217,7 @@ inline namespace kernel
 
       library.define<procedure>("eval", [](let const& xs)
       {
-        return cadr(xs).as<environment>().evaluate(car(xs));
+        return xs[1].as<environment>().evaluate(xs[0]);
       });
 
       library.define<procedure>("interaction-environment", []()
@@ -227,7 +227,7 @@ inline namespace kernel
 
       library.define<procedure>("load", [](let const& xs)
       {
-        return car(xs).as<environment>().load(cadr(xs).as<string>());
+        return xs[0].as<environment>().load(xs[1].as<string>());
       });
     });
 
@@ -235,32 +235,32 @@ inline namespace kernel
     {
       library.define<procedure>("throw", [](let const& xs) -> object
       {
-        throw car(xs);
+        throw xs[0];
       });
 
       library.define<procedure>("error-object", [](let const& xs)
       {
-        return make<error>(car(xs), cdr(xs));
+        return make<error>(xs[0], cdr(xs));
       });
 
       library.define<procedure>("error-object?", [](let const& xs)
       {
-        return car(xs).is_also<error>();
+        return xs[0].is_also<error>();
       });
 
       library.define<procedure>("read-error?", [](let const& xs)
       {
-        return car(xs).is<read_error>();
+        return xs[0].is<read_error>();
       });
 
       library.define<procedure>("file-error?", [](let const& xs)
       {
-        return car(xs).is<file_error>();
+        return xs[0].is<file_error>();
       });
 
       library.define<procedure>("kernel-exception-handler-set!", [](let const& xs)
       {
-        return environment::raise = car(xs);
+        return environment::raise = xs[0];
       });
     });
 
@@ -268,14 +268,14 @@ inline namespace kernel
     {
       library.define<procedure>("type-of", [](let const& xs)
       {
-        std::cout << car(xs).type().name() << std::endl;
+        std::cout << xs[0].type().name() << std::endl;
       });
 
       library.define<procedure>("disassemble", [](let const& xs)
       {
         if (0 < length(xs))
         {
-          if (let const& f = car(xs); f.is<closure>())
+          if (let const& f = xs[0]; f.is<closure>())
           {
             disassemble(std::cout, car(f));
           }
@@ -315,22 +315,22 @@ inline namespace kernel
     {
       library.define<procedure>("closure?", [](let const& xs)
       {
-        return car(xs).is<closure>();
+        return xs[0].is<closure>();
       });
 
       library.define<procedure>("continuation?", [](let const& xs)
       {
-        return car(xs).is<continuation>();
+        return xs[0].is<continuation>();
       });
 
       library.define<procedure>("foreign-function", [](let const& xs)
       {
-        return make<procedure>(cadr(xs).as<string>(), car(xs).as<string>());
+        return make<procedure>(xs[1].as<string>(), xs[0].as<string>());
       });
 
       library.define<procedure>("foreign-function?", [](let const& xs)
       {
-        return car(xs).is<procedure>();
+        return xs[0].is<procedure>();
       });
     });
 
@@ -446,7 +446,7 @@ inline namespace kernel
     {
       library.define<procedure>("null?", [](let const& xs)
       {
-        return car(xs).is<null>();
+        return xs[0].is<null>();
       });
 
       library.define<procedure>("append", [](let const& xs)
@@ -510,22 +510,22 @@ inline namespace kernel
 
       library.define<procedure>("identifier?", [](let const& xs)
       {
-        return car(xs).is_also<identifier>();
+        return xs[0].is_also<identifier>();
       });
 
       library.define<procedure>("transformer?", [](let const& xs)
       {
-        return car(xs).is<transformer>();
+        return xs[0].is<transformer>();
       });
 
       library.define<procedure>("syntactic-closure?", [](let const& xs)
       {
-        return car(xs).is<syntactic_closure>();
+        return xs[0].is<syntactic_closure>();
       });
 
       library.define<procedure>("make-syntactic-closure", [](let const& xs)
       {
-        return make<syntactic_closure>(car(xs), cadr(xs), caddr(xs));
+        return make<syntactic_closure>(xs[0], xs[1], xs[2]);
       });
     });
 
@@ -647,12 +647,12 @@ inline namespace kernel
 
       library.define<procedure>("ratio-numerator", [](let const& xs)
       {
-        return make(car(xs).as<ratio>().numerator());
+        return make(xs[0].as<ratio>().numerator());
       });
 
       library.define<procedure>("ratio-denominator", [](let const& xs)
       {
-        return make(car(xs).as<ratio>().denominator());
+        return make(xs[0].as<ratio>().denominator());
       });
 
       library.define<procedure>("floor", [](let const& xs)
@@ -714,50 +714,50 @@ inline namespace kernel
     {
       library.define<procedure>("pair?", [](let const& xs)
       {
-        return car(xs).is<pair>();
+        return xs[0].is<pair>();
       });
 
       library.define<procedure>("cons", [](let const& xs)
       {
-        return cons(car(xs), cadr(xs));
+        return cons(xs[0], xs[1]);
       });
 
-      library.define<procedure>("car", [](let const& xs) { return caar(xs); });
-      library.define<procedure>("cdr", [](let const& xs) { return cdar(xs); });
+      library.define<procedure>("car", [](let const& xs) { return car(xs[0]); });
+      library.define<procedure>("cdr", [](let const& xs) { return cdr(xs[0]); });
 
-      library.define<procedure>("caar", [](let const& xs) { return caar(car(xs)); });
-      library.define<procedure>("cadr", [](let const& xs) { return cadr(car(xs)); });
-      library.define<procedure>("cdar", [](let const& xs) { return cdar(car(xs)); });
-      library.define<procedure>("cddr", [](let const& xs) { return cddr(car(xs)); });
+      library.define<procedure>("caar", [](let const& xs) { return caar(xs[0]); });
+      library.define<procedure>("cadr", [](let const& xs) { return cadr(xs[0]); });
+      library.define<procedure>("cdar", [](let const& xs) { return cdar(xs[0]); });
+      library.define<procedure>("cddr", [](let const& xs) { return cddr(xs[0]); });
 
-      library.define<procedure>("caaar", [](let const& xs) { return caaar(car(xs)); });
-      library.define<procedure>("caadr", [](let const& xs) { return caadr(car(xs)); });
-      library.define<procedure>("cadar", [](let const& xs) { return cadar(car(xs)); });
-      library.define<procedure>("caddr", [](let const& xs) { return caddr(car(xs)); });
-      library.define<procedure>("cdaar", [](let const& xs) { return cdaar(car(xs)); });
-      library.define<procedure>("cdadr", [](let const& xs) { return cdadr(car(xs)); });
-      library.define<procedure>("cddar", [](let const& xs) { return cddar(car(xs)); });
-      library.define<procedure>("cdddr", [](let const& xs) { return cdddr(car(xs)); });
+      library.define<procedure>("caaar", [](let const& xs) { return caaar(xs[0]); });
+      library.define<procedure>("caadr", [](let const& xs) { return caadr(xs[0]); });
+      library.define<procedure>("cadar", [](let const& xs) { return cadar(xs[0]); });
+      library.define<procedure>("caddr", [](let const& xs) { return caddr(xs[0]); });
+      library.define<procedure>("cdaar", [](let const& xs) { return cdaar(xs[0]); });
+      library.define<procedure>("cdadr", [](let const& xs) { return cdadr(xs[0]); });
+      library.define<procedure>("cddar", [](let const& xs) { return cddar(xs[0]); });
+      library.define<procedure>("cdddr", [](let const& xs) { return cdddr(xs[0]); });
 
-      library.define<procedure>("caaaar", [](let const& xs) { return caaaar(car(xs)); });
-      library.define<procedure>("caaadr", [](let const& xs) { return caaadr(car(xs)); });
-      library.define<procedure>("caadar", [](let const& xs) { return caadar(car(xs)); });
-      library.define<procedure>("caaddr", [](let const& xs) { return caaddr(car(xs)); });
-      library.define<procedure>("cadaar", [](let const& xs) { return cadaar(car(xs)); });
-      library.define<procedure>("cadadr", [](let const& xs) { return cadadr(car(xs)); });
-      library.define<procedure>("caddar", [](let const& xs) { return caddar(car(xs)); });
-      library.define<procedure>("cadddr", [](let const& xs) { return cadddr(car(xs)); });
-      library.define<procedure>("cdaaar", [](let const& xs) { return cdaaar(car(xs)); });
-      library.define<procedure>("cdaadr", [](let const& xs) { return cdaadr(car(xs)); });
-      library.define<procedure>("cdadar", [](let const& xs) { return cdadar(car(xs)); });
-      library.define<procedure>("cdaddr", [](let const& xs) { return cdaddr(car(xs)); });
-      library.define<procedure>("cddaar", [](let const& xs) { return cddaar(car(xs)); });
-      library.define<procedure>("cddadr", [](let const& xs) { return cddadr(car(xs)); });
-      library.define<procedure>("cdddar", [](let const& xs) { return cdddar(car(xs)); });
-      library.define<procedure>("cddddr", [](let const& xs) { return cddddr(car(xs)); });
+      library.define<procedure>("caaaar", [](let const& xs) { return caaaar(xs[0]); });
+      library.define<procedure>("caaadr", [](let const& xs) { return caaadr(xs[0]); });
+      library.define<procedure>("caadar", [](let const& xs) { return caadar(xs[0]); });
+      library.define<procedure>("caaddr", [](let const& xs) { return caaddr(xs[0]); });
+      library.define<procedure>("cadaar", [](let const& xs) { return cadaar(xs[0]); });
+      library.define<procedure>("cadadr", [](let const& xs) { return cadadr(xs[0]); });
+      library.define<procedure>("caddar", [](let const& xs) { return caddar(xs[0]); });
+      library.define<procedure>("cadddr", [](let const& xs) { return cadddr(xs[0]); });
+      library.define<procedure>("cdaaar", [](let const& xs) { return cdaaar(xs[0]); });
+      library.define<procedure>("cdaadr", [](let const& xs) { return cdaadr(xs[0]); });
+      library.define<procedure>("cdadar", [](let const& xs) { return cdadar(xs[0]); });
+      library.define<procedure>("cdaddr", [](let const& xs) { return cdaddr(xs[0]); });
+      library.define<procedure>("cddaar", [](let const& xs) { return cddaar(xs[0]); });
+      library.define<procedure>("cddadr", [](let const& xs) { return cddadr(xs[0]); });
+      library.define<procedure>("cdddar", [](let const& xs) { return cdddar(xs[0]); });
+      library.define<procedure>("cddddr", [](let const& xs) { return cddddr(xs[0]); });
 
-      library.define<procedure>("set-car!", [](auto&& xs) { return caar(xs) = cadr(xs); });
-      library.define<procedure>("set-cdr!", [](auto&& xs) { return cdar(xs) = cadr(xs); });
+      library.define<procedure>("set-car!", [](auto&& xs) { return car(xs[0]) = xs[1]; });
+      library.define<procedure>("set-cdr!", [](auto&& xs) { return cdr(xs[0]) = xs[1]; });
     });
 
     define<library>("(meevax port)", [](library & library)
@@ -935,7 +935,7 @@ inline namespace kernel
     {
       library.define<procedure>("string?", [](let const& xs)
       {
-        return car(xs).is<string>();
+        return xs[0].is<string>();
       });
 
       library.define<procedure>("make-string", [](let const& xs)
@@ -977,7 +977,7 @@ inline namespace kernel
            execute in constant time.
         */
 
-        return make(car(xs).as<string>().codepoints.at(cadr(xs).as<exact_integer>()));
+        return make(xs[0].as<string>().codepoints.at(xs[1].as<exact_integer>()));
       });
 
       library.define<procedure>("string-set!", [](let const& xs)
@@ -991,9 +991,9 @@ inline namespace kernel
            is no requirement for this procedure to execute in constant time.
         */
 
-        car(xs).as<string>().codepoints.at(cadr(xs).as<exact_integer>()) = caddr(xs).as<character>();
+        xs[0].as<string>().codepoints.at(xs[1].as<exact_integer>()) = xs[2].as<character>();
 
-        return car(xs);
+        return xs[0];
       });
 
       library.define<procedure>("string-append", [](let const& xs)
@@ -1089,7 +1089,7 @@ inline namespace kernel
 
       library.define<procedure>("symbol->string", [](let const& xs)
       {
-        return make<string>(car(xs).as<symbol>());
+        return make<string>(xs[0].as<symbol>());
       });
 
       library.define<procedure>("number->string", [](let const& xs)
@@ -1157,19 +1157,19 @@ inline namespace kernel
     {
       library.define<procedure>("symbol?", [](let const& xs)
       {
-        return car(xs).is<symbol>();
+        return xs[0].is<symbol>();
       });
 
       library.define<procedure>("string->symbol", [](let const& xs)
       {
-        return make_symbol(car(xs).as<string>());
+        return make_symbol(xs[0].as<string>());
       });
 
       using syntactic_closure = environment::syntactic_closure;
 
       library.define<procedure>("identifier->symbol", [](let const& xs)
       {
-        if (let const& x = car(xs); x.is<syntactic_closure>())
+        if (let const& x = xs[0]; x.is<syntactic_closure>())
         {
           return x.as<syntactic_closure>().expression;
         }
@@ -1385,7 +1385,7 @@ inline namespace kernel
 
       library.define<procedure>("list->vector", [](let const& xs)
       {
-        return make<vector>(car(xs));
+        return make<vector>(xs[0]);
       });
 
       library.define<procedure>("string->vector", [](let const& xs)
