ea10ec1f36e2cbbd3afaea50a368333fc65597b1
yamacir-kit <httperror@404-notfound.jp>
2026-05-27 00:12:19 +0900

    Cleanup header `kernel/collector.hpp`
    Signed-off-by: yamacir-kit <httperror@404-notfound.jp>

--- include/meevax/kernel/collector.hpp
+++ include/meevax/kernel/collector.hpp
@@ -27,169 +27,167 @@
 
 namespace meevax::inline kernel
 {
+  auto clear() -> void;
+
+  auto clear_once() -> void;
+
+  auto collect() -> void;
+
+  auto count() -> std::size_t;
+
+  auto insert(pair const*) -> void;
+
+  auto is_root(object const*) noexcept -> bool;
+
+  auto request(std::size_t) -> void;
+
+  auto reserve(std::size_t) -> void;
+
+  template <typename A>
+  struct stateful : public A
+  {
+    ~stateful()
+    {
+      /*
+         Execute clear before any static allocator is destroyed. Otherwise,
+         when the destructor of the collector executes clear, the collector may
+         touch the freed memory of the stateful allocator.
+      */
+      clear_once();
+    }
+  };
+
   inline constexpr struct with_braces_tag {} with_braces {};
 
-    auto clear() -> void;
+  template <typename Bound, typename A>
+  struct binder final : public virtual pair
+                      , public Bound
+  {
+    using allocator = stateful<typename std::allocator_traits<A>::template rebind_alloc<binder>>;
 
-    auto clear_once() -> void;
+    auto static inline a = allocator();
 
-    template <typename A>
-    struct stateful : public A
+    explicit binder(auto&&... xs)
+      : std::conditional_t<std::is_base_of_v<pair, Bound> and std::is_constructible_v<pair, decltype(xs)...>, pair, Bound>(std::forward<decltype(xs)>(xs)...)
+    {}
+
+    explicit binder(with_braces_tag, auto&&... xs)
+      : std::conditional_t<std::is_base_of_v<pair, Bound> and std::is_constructible_v<pair, decltype(xs)...>, pair, Bound> { std::forward<decltype(xs)>(xs)... }
+    {}
+
+    ~binder() override = default;
+
+    auto eqv([[maybe_unused]] pair const* other) const -> bool override
     {
-      ~stateful()
+      if constexpr (std::equality_comparable<Bound const&>)
       {
-        /*
-           Execute clear before any static allocator is destroyed. Otherwise,
-           when the destructor of the collector executes clear, the collector
-           may touch the freed memory of the stateful allocator.
-        */
-        clear_once();
-      }
-    };
-
-    template <typename Bound, typename A>
-    struct binder final : public virtual pair
-                        , public Bound
-    {
-      using allocator = stateful<typename std::allocator_traits<A>::template rebind_alloc<binder>>;
-
-      auto static inline a = allocator();
-
-      explicit binder(auto&&... xs)
-        : std::conditional_t<std::is_base_of_v<pair, Bound> and std::is_constructible_v<pair, decltype(xs)...>, pair, Bound>(std::forward<decltype(xs)>(xs)...)
-      {}
-
-      explicit binder(with_braces_tag, auto&&... xs)
-        : std::conditional_t<std::is_base_of_v<pair, Bound> and std::is_constructible_v<pair, decltype(xs)...>, pair, Bound> { std::forward<decltype(xs)>(xs)... }
-      {}
-
-      ~binder() override = default;
-
-      auto eqv([[maybe_unused]] pair const* other) const -> bool override
-      {
-        if constexpr (std::equality_comparable<Bound const&>)
+        if (auto const* bound = dynamic_cast<Bound const*>(other); bound)
         {
-          if (auto const* bound = dynamic_cast<Bound const*>(other); bound)
-          {
-            return this == bound or *static_cast<Bound const*>(this) == *bound;
-          }
-          else
-          {
-            return std::is_same_v<Bound, std::nullptr_t>;
-          }
+          return this == bound or *static_cast<Bound const*>(this) == *bound;
         }
         else
         {
-          return false;
-        }
-      }
-
-      auto extent() const noexcept -> std::pair<void const*, std::size_t> override
-      {
-        return { static_cast<Bound const*>(this), sizeof(Bound) };
-      }
-
-      auto contains(void const* p) const noexcept -> bool override
-      {
-        auto base = static_cast<Bound const*>(this);
-        return base <= p and p < reinterpret_cast<void const*>(reinterpret_cast<std::uintptr_t>(base) + sizeof(Bound));
-      }
-
-      auto type() const noexcept -> std::type_info const& override
-      {
-        return typeid(Bound);
-      }
-
-      auto write(std::ostream & os) const -> std::ostream & override
-      {
-        if constexpr (requires { os << static_cast<Bound const&>(*this); })
-        {
-          return os << static_cast<Bound const&>(*this);
-        }
-        else
-        {
-          return os << magenta("#,(") << green(typeid(Bound).name()) << faint(" #;", static_cast<Bound const*>(this)) << magenta(")");
-        }
-      }
-
-      auto operator new(std::size_t) -> void *
-      {
-        return a.allocate(1);
-      }
-
-      auto operator delete(void * data) noexcept -> void
-      {
-        a.deallocate(reinterpret_cast<typename std::allocator_traits<allocator>::pointer>(data), 1);
-      }
-    };
-
-    template <typename A>
-    struct binder<pair, A> final : public pair
-    {
-      using allocator = stateful<typename std::allocator_traits<A>::template rebind_alloc<binder>>;
-
-      auto static inline a = allocator();
-
-      using pair::pair;
-
-      ~binder() override = default;
-
-      auto operator new(std::size_t) -> void *
-      {
-        return a.allocate(1);
-      }
-
-      auto operator delete(void * data) noexcept -> void
-      {
-        a.deallocate(reinterpret_cast<typename std::allocator_traits<allocator>::pointer>(data), 1);
-      }
-    };
-
-    /*
-       https://www.kernel.org/doc/html/latest/arch/x86/x86_64/mm.html
-
-       0x0000'0000'0000'0000 ~ 0x0000'7FFF'FFFF'FFFF
-    */
-    template <typename T>
-    using canonical_pointer_set = pointer_set<T const*, std::bit_width(0x7FFFu),
-                                                        std::bit_width(0xFFFFu),
-                                                        std::bit_width(0xFFFFu)>;
-
-    auto collect() -> void;
-
-    auto count() -> std::size_t;
-
-    auto request(std::size_t) -> void;
-
-    auto reserve(std::size_t) -> void;
-
-    auto insert(pair const*) -> void;
-
-    auto is_root(object const*) noexcept -> bool;
-
-    template <typename T, typename A = std::conditional_t<std::is_same_v<T, pair>, segregated_storage_allocator<void>, std::allocator<void>>>
-    auto make(auto&&... xs) -> object
-    {
-      if constexpr (std::is_class_v<T>)
-      {
-        request(sizeof(T));
-
-        if (auto datum = new binder<T, A>(std::forward<decltype(xs)>(xs)...); datum)
-        {
-          insert(datum);
-
-          return datum;
-        }
-        else [[unlikely]]
-        {
-          throw std::bad_alloc();
+          return std::is_same_v<Bound, std::nullptr_t>;
         }
       }
       else
       {
-        return { std::forward<decltype(xs)>(xs)... };
+        return false;
       }
     }
+
+    auto extent() const noexcept -> std::pair<void const*, std::size_t> override
+    {
+      return { static_cast<Bound const*>(this), sizeof(Bound) };
+    }
+
+    auto contains(void const* p) const noexcept -> bool override
+    {
+      auto base = static_cast<Bound const*>(this);
+      return base <= p and p < reinterpret_cast<void const*>(reinterpret_cast<std::uintptr_t>(base) + sizeof(Bound));
+    }
+
+    auto type() const noexcept -> std::type_info const& override
+    {
+      return typeid(Bound);
+    }
+
+    auto write(std::ostream & os) const -> std::ostream & override
+    {
+      if constexpr (requires { os << static_cast<Bound const&>(*this); })
+      {
+        return os << static_cast<Bound const&>(*this);
+      }
+      else
+      {
+        return os << magenta("#,(") << green(typeid(Bound).name()) << faint(" #;", static_cast<Bound const*>(this)) << magenta(")");
+      }
+    }
+
+    auto operator new(std::size_t) -> void *
+    {
+      return a.allocate(1);
+    }
+
+    auto operator delete(void * data) noexcept -> void
+    {
+      a.deallocate(reinterpret_cast<typename std::allocator_traits<allocator>::pointer>(data), 1);
+    }
+  };
+
+  template <typename A>
+  struct binder<pair, A> final : public pair
+  {
+    using allocator = stateful<typename std::allocator_traits<A>::template rebind_alloc<binder>>;
+
+    auto static inline a = allocator();
+
+    using pair::pair;
+
+    ~binder() override = default;
+
+    auto operator new(std::size_t) -> void *
+    {
+      return a.allocate(1);
+    }
+
+    auto operator delete(void * data) noexcept -> void
+    {
+      a.deallocate(reinterpret_cast<typename std::allocator_traits<allocator>::pointer>(data), 1);
+    }
+  };
+
+  template <typename T, typename A = std::conditional_t<std::is_same_v<T, pair>, segregated_storage_allocator<void>, std::allocator<void>>>
+  auto make(auto&&... xs) -> object
+  {
+    if constexpr (std::is_class_v<T>)
+    {
+      request(sizeof(T));
+
+      if (auto datum = new binder<T, A>(std::forward<decltype(xs)>(xs)...); datum)
+      {
+        insert(datum);
+
+        return datum;
+      }
+      else [[unlikely]]
+      {
+        throw std::bad_alloc();
+      }
+    }
+    else
+    {
+      return { std::forward<decltype(xs)>(xs)... };
+    }
+  }
+
+  /*
+     https://www.kernel.org/doc/html/latest/arch/x86/x86_64/mm.html
+
+     0x0000'0000'0000'0000 ~ 0x0000'7FFF'FFFF'FFFF
+  */
+  template <typename T>
+  using canonical_pointer_set = pointer_set<T const*, std::bit_width(0x7FFFu), std::bit_width(0xFFFFu), std::bit_width(0xFFFFu)>;
 
 namespace backdoor
 {
